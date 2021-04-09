e228a31ed038d3acd6fa1abde17483e54673abb0
kpeakers <60412112+kpeakers@users.noreply.github.com>
2021-04-09 16:10:26 -0600

    got the tattoo showing up! should be just repeating that step from here, a lot of positioning

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,58 @@
-console.log ("running");
+console.log("running");
 
 var vidcapture, ctracker, drawcanvas;
 
-let css;
+var css;
 
 function preload() {
-    
+
     css = loadImage('imgs/css.png');
 }
 
 
 function setup() {
 
-    var cnv = createCanvas(windowWidth, windowHeight /2);
+    var cnv = createCanvas(windowWidth, windowHeight / 2);
     cnv.parent("p5canvas");
-//    background(255, 0, 0);
-    
+    //    background(255, 0, 0);
+
     //p5 method for creating a video stream
     vidcapture = createCapture(VIDEO);
-    vidcapture.size(vidcapture.width*2.5, vidcapture.height*3.8);
+    vidcapture.size(vidcapture.width * 2.5, vidcapture.height * 3.8);
     vidcapture.hide();
 
     //start the tracker 
     ctracker = new clm.tracker();
     ctracker.init();
     ctracker.start(vidcapture.elt);
-    
+
     //just for testing
-    drawcanvas = document.getElementById('defaultCanvas0');
+    //    drawcanvas = document.getElementById('defaultCanvas0');
     //defaultcanvas0 is the default name for p5
-    
-    image(css, 0, 0);
-    console.log('css');
+
+
 }
 
 function draw() {
-    
-    //unflipping canvas
-    translate(vidcapture.width, 0);
-    scale(-1, 1);
-    
-    
-    
-  var position = ctracker.getCurrentPosition();
-    
-      image(vidcapture, 0, 0);
-        //actual feed DONT TOUCH NUMBERS KAYLEE  image(vidcapture, 0, 0);
-        //actual feed
-    
-    if (position){
-        
-        ctracker.draw(drawcanvas);
-        //this draws initial face outline
 
-//        
-////        position.forEach(function(pos){
-////            
-////        })
-//        // use this to apply something to ALL coordinates on chart
-   
-        
+    image(vidcapture, 0, 0);
+    //actual feed DONT TOUCH NUMBERS KAYLEE  
+
+    var position = ctracker.getCurrentPosition();
+
+    print(position);
+
+    if (position) {
+
+        //        scale(-1, 1);
+        image(css, position[62][0], position[62][1], 50, 50);
+
+        //        fill(0);
+        //        ellipse(position[62][0], position[62][1], 10);
+        //        console.log('dot');
+
+        //                ctracker.draw(drawcanvas);
+        //this draws initial face outline
 
     }
 }
-
